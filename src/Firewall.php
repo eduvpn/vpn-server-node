135eaf63f2d7f0c0ea18edfe76daae8cb5357fc1
François Kooman <fkooman@tuxed.net>
2019-02-18 15:36:17 +0100

    simplify firewall

--- src/Firewall.php
+++ src/Firewall.php
@@ -67,19 +67,6 @@ class Firewall
         // INPUT
         $firewall = array_merge($firewall, self::getInputChain($inetFamily, $firewallConfig));
 
-        // FORWARD
-        $firewall = array_merge(
-            $firewall,
-            [
-                sprintf('-A FORWARD -p %s -j ACCEPT', 4 === $inetFamily ? 'icmp' : 'ipv6-icmp'),
-                '-A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT',
-            ]
-        );
-
-        $firewall = array_merge($firewall, self::getForwardChain($firewallConfig, $profileList, $inetFamily));
-        $firewall[] = sprintf('-A FORWARD -j REJECT --reject-with %s', 4 === $inetFamily ? 'icmp-host-prohibited' : 'icmp6-adm-prohibited');
-        $firewall[] = 'COMMIT';
-
         return array_merge(
             [
                 '#',
@@ -101,34 +88,28 @@ class Firewall
      */
     private static function getNat(Config $firewallConfig, array $profileList, $inetFamily)
     {
-        if (!$firewallConfig->hasSection('forwardChain')) {
-            return [];
+        $natRules = [];
+
+        if (!$firewallConfig->hasSection('natConfig')) {
+            return $natRules;
         }
 
-        $nat = [];
-        foreach ($profileList as $profileId => $profileData) {
-            if (!$firewallConfig->getSection('forwardChain')->hasSection($profileId)) {
-                // this profile has no configuration in "forwardChain"
+        // loop over all profiles
+        $natProfileList = $firewallConfig->getSection('natConfig')->optionalItem('profileList', []);
+        foreach ($natProfileList as $profileId) {
+            if (!array_key_exists($profileId, $profileList)) {
                 continue;
             }
-            $profileConfig = new ProfileConfig($profileData);
-            $profileFirewallConfig = $firewallConfig->getSection('forwardChain')->getSection($profileId);
-
-            $enableNat4 = $profileFirewallConfig->optionalItem('enableNat4', false);
-            $enableNat6 = $profileFirewallConfig->optionalItem('enableNat6', false);
-
-            if ($enableNat4 && 4 === $inetFamily) {
-                $srcNet = $profileConfig->getItem('range');
-                $nat[] = sprintf('-A POSTROUTING -s %s -o %s -j MASQUERADE', $srcNet, $profileFirewallConfig->getItem('extIf'));
-            }
-
-            if ($enableNat6 && 6 === $inetFamily) {
-                $srcNet = $profileConfig->getItem('range6');
-                $nat[] = sprintf('-A POSTROUTING -s %s -o %s -j MASQUERADE', $srcNet, $profileFirewallConfig->getItem('extIf'));
-            }
+            $profileConfig = new ProfileConfig($profileList[$profileId]);
+            $srcNet = 4 === $inetFamily ? $profileConfig->getItem('range') : $profileConfig->getItem('range6');
+            $natRules[] = sprintf(
+                '-A POSTROUTING -s %s -o %s -j MASQUERADE',
+                $srcNet,
+                $firewallConfig->getSection('natConfig')->getItem('extIf')
+            );
         }
 
-        return $nat;
+        return $natRules;
     }
 
     /**
@@ -139,124 +120,45 @@ class Firewall
     private static function getInputChain($inetFamily, Config $firewallConfig)
     {
         $inputChain = [
-            '-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT',
-            sprintf('-A INPUT -p %s -j ACCEPT', 4 === $inetFamily ? 'icmp' : 'ipv6-icmp'),
             '-A INPUT -i lo -j ACCEPT',
+            sprintf('-A INPUT -p %s -j ACCEPT', 4 === $inetFamily ? 'icmp' : 'ipv6-icmp'),
+            '-A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT',
         ];
 
-        $udpPorts = $firewallConfig->getSection('inputChain')->getSection('udp')->toArray();
-        $tcpPorts = $firewallConfig->getSection('inputChain')->getSection('tcp')->toArray();
+        $inputRules = $firewallConfig->getSection('inputRules')->toArray();
+        foreach ($inputRules as $inputRule) {
+            $protoList = $inputRule['proto'];
+            $srcNetList = array_key_exists('src_net', $inputRule) ? $inputRule['src_net'] : [];
+            $dstPortList = $inputRule['dst_port'];
 
-        foreach ($udpPorts as $udpPort) {
-            if (!\is_array($udpPort)) {
-                $inputChain[] = sprintf(
-                    '-A INPUT -m state --state NEW -m udp -p udp --dport %s -j ACCEPT',
-                    $udpPort
-                );
-
-                continue;
-            }
-
-            foreach ($udpPort['src'] as $src) {
-                $ipSource = new IP($src);
-                if ($inetFamily === $ipSource->getFamily()) {
-                    $inputChain[] = sprintf(
-                        '-A INPUT -m state --state NEW -m udp -p udp --source %s --dport %s -j ACCEPT',
-                        $src,
-                        $udpPort['port']
-                    );
-                }
-            }
-        }
-
-        foreach ($tcpPorts as $tcpPort) {
-            if (!\is_array($tcpPort)) {
-                $inputChain[] = sprintf(
-                    '-A INPUT -m state --state NEW -m tcp -p tcp --dport %s -j ACCEPT',
-                    $tcpPort
-                );
-
-                continue;
-            }
-
-            foreach ($tcpPort['src'] as $src) {
-                $ipSource = new IP($src);
-                if ($inetFamily === $ipSource->getFamily()) {
-                    $inputChain[] = sprintf(
-                        '-A INPUT -m state --state NEW -m tcp -p tcp --source %s --dport %s -j ACCEPT',
-                        $src,
-                        $tcpPort['port']
-                    );
-                }
-            }
-        }
-
-        $inputChain[] = sprintf('-A INPUT -j REJECT --reject-with %s', 4 === $inetFamily ? 'icmp-host-prohibited' : 'icmp6-adm-prohibited');
-
-        return $inputChain;
-    }
-
-    /**
-     * @param int $inetFamily
-     *
-     * @return array
-     */
-    private static function getForwardChain(Config $firewallConfig, array $profileList, $inetFamily)
-    {
-        if (!$firewallConfig->hasSection('forwardChain')) {
-            return [];
-        }
-
-        $forwardChain = [];
-        foreach ($profileList as $profileId => $profileData) {
-            if (!$firewallConfig->getSection('forwardChain')->hasSection($profileId)) {
-                // this profile has no configuration in "forwardChain"
-                continue;
-            }
-
-            $profileFirewallConfig = $firewallConfig->getSection('forwardChain')->getSection($profileId);
-            $profileConfig = new ProfileConfig($profileData);
-            $profileNumber = $profileConfig->getItem('profileNumber');
-
-            if (4 === $inetFamily && $profileFirewallConfig->optionalItem('reject4', false)) {
-                // IPv4 forwarding is disabled
-                continue;
-            }
-
-            if (6 === $inetFamily && $profileFirewallConfig->optionalItem('reject6', false)) {
-                // IPv6 forwarding is disabled
-                continue;
-            }
-
-            if (4 === $inetFamily) {
-                // get the IPv4 range
-                $srcNet = $profileConfig->getItem('range');
-            } else {
-                // get the IPv6 range
-                $srcNet = $profileConfig->getItem('range6');
-            }
-            $forwardChain[] = sprintf('-N vpn-%s', $profileNumber);
-
-            $forwardChain[] = sprintf('-A FORWARD -i tun-%s+ -s %s -j vpn-%s', $profileNumber, $srcNet, $profileNumber);
-
-            if ($profileConfig->getItem('clientToClient')) {
-                // allow client-to-client
-                $forwardChain[] = sprintf('-A vpn-%s -o tun-%s+ -d %s -j ACCEPT', $profileNumber, $profileNumber, $srcNet);
-            }
-            if ($profileConfig->getItem('defaultGateway')) {
-                // allow traffic to all outgoing destinations
-                $forwardChain[] = sprintf('-A vpn-%s -o %s -j ACCEPT', $profileNumber, $profileFirewallConfig->getItem('extIf'));
-            } else {
-                // only allow certain traffic to the external interface
-                foreach ($profileConfig->getSection('routes')->toArray() as $route) {
-                    $routeIp = new IP($route);
-                    if ($inetFamily === $routeIp->getFamily()) {
-                        $forwardChain[] = sprintf('-A vpn-%s -o %s -d %s -j ACCEPT', $profileNumber, $profileFirewallConfig->getItem('extIf'), $route);
+            foreach ($protoList as $proto) {
+                foreach ($dstPortList as $dstPort) {
+                    if (0 === \count($srcNetList)) {
+                        $inputChain[] = sprintf(
+                            '-A INPUT -m state --state NEW -m %s -p %s --dport %s -j ACCEPT',
+                            $proto,
+                            $proto,
+                            $dstPort
+                        );
+                        continue;
+                    }
+                    foreach ($srcNetList as $srcNet) {
+                        $srcNetFamily = false === strpos($srcNet, ':') ? 4 : 6;
+                        if ($srcNetFamily === $inetFamily) {
+                            $inputChain[] = sprintf(
+                                '-A INPUT -m state --state NEW -m %s -p %s --source %s --dport %s -j ACCEPT',
+                                $proto,
+                                $proto,
+                                $srcNet,
+                                $dstPort
+                            );
+                        }
                     }
                 }
             }
         }
+        $inputChain[] = sprintf('-A INPUT -j REJECT --reject-with %s', 4 === $inetFamily ? 'icmp-host-prohibited' : 'icmp6-adm-prohibited');
 
-        return $forwardChain;
+        return $inputChain;
     }
 }
