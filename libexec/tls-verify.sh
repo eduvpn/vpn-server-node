4f6508cf564a649ca966bb87392783968820cbd0
François Kooman <fkooman@tuxed.net>
2021-05-12 18:01:07 +0200

    update some comments/error messages

--- libexec/tls-verify.sh
+++ libexec/tls-verify.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-# OpenVPN script to be used by --tls-verify option.
-# OpenVPN executes script for each certificate in the client's certificate-chain
-# We are only interested in the client certificate (depth 0)
+# OpenVPN --tls-verify script
+#
+# OpenVPN executes this script multiple times for each certificate in the 
+# chain. We are only interested in the client certificate (depth 0)
+
 if [ "${1}" -eq 0 ]; then
     if [ -z "${PROFILE_ID}" ] || [ -z "${X509_0_OU}" ]; then
-        logger -s -p user.warning "${0}: PROFILE_ID OR X509_0_OU not set"
+        /usr/bin/logger -s -p user.warning "${0}: PROFILE_ID or X509_0_OU environment variable not set"
         exit 1
     fi
 
     if [ "${PROFILE_ID}" != "${X509_0_OU}" ]; then
-        logger -s -p user.warning "${0}: PROFILE_ID '${PROFILE_ID}' does not match client's certificate OU '${X509_0_OU}'"
+        /usr/bin/logger -s -p user.warning "${0}: PROFILE_ID '${PROFILE_ID}' does not match client certificate OU '${X509_0_OU}'"
         exit 1
     fi
 fi
