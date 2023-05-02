a3ac704d386af8c9ef7f0745556d7534f186071f
François Kooman <fkooman@tuxed.net>
2023-05-02 17:46:35 +0200

    source formatting

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -17,6 +17,12 @@ return $config->setRules(
         'ordered_imports' => true,
         // Orders the elements of classes/interfaces/traits/enums.
         'ordered_class_elements' => true,
+        // Ensures a single space after language constructs.
+        'single_space_around_construct' => true,
+        // An empty line feed must precede any configured statement.
+        'blank_line_before_statement' => true,
+        // Functions should be used with $strict param set to true.
+        'strict_param' => true,
         // Annotations in PHPDoc should be grouped together so that annotations
         // of the same type immediately follow each other. Annotations of a
         // different type are separated by a single blank line.
@@ -26,7 +32,7 @@ return $config->setRules(
             'header' => <<< 'EOD'
                 eduVPN - End-user friendly VPN.
 
-                Copyright: 2016-2022, The Commons Conservancy eduVPN Programme
+                Copyright: 2016-2023, The Commons Conservancy eduVPN Programme
                 SPDX-License-Identifier: AGPL-3.0+
                 EOD,
         ],
